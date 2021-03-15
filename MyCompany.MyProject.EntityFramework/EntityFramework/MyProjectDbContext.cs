823e217fcd393b2f0179c7019036bbffb05cd5d9
Jhang jia syun <lyen51032@gmail.com>
2021-03-15 14:31:39 +0800

    feat:DbContest連結/Map建立

--- MyCompany.MyProject.EntityFramework/EntityFramework/MyProjectDbContext.cs
+++ MyCompany.MyProject.EntityFramework/EntityFramework/MyProjectDbContext.cs
@@ -4,6 +4,7 @@ using Abp.DynamicEntityProperties;
 using Abp.Zero.EntityFramework;
 using MyCompany.MyProject.Authorization.Roles;
 using MyCompany.MyProject.Authorization.Users;
+using MyCompany.MyProject.Entities;
 using MyCompany.MyProject.MultiTenancy;
 
 namespace MyCompany.MyProject.EntityFramework
@@ -11,6 +12,9 @@ namespace MyCompany.MyProject.EntityFramework
     public class MyProjectDbContext : AbpZeroDbContext<Tenant, Role, User>
     {
         //TODO: Define an IDbSet for your Entities...
+        public virtual IDbSet<Player> Players { set; get; }
+        public virtual IDbSet<Map> Maps { set; get; }
+
 
         /* NOTE: 
          *   Setting "Default" to base class helps us when working migration commands on Package Manager Console.
@@ -52,6 +56,13 @@ namespace MyCompany.MyProject.EntityFramework
 
             modelBuilder.Entity<DynamicProperty>().Property(p => p.PropertyName).HasMaxLength(250);
             modelBuilder.Entity<DynamicEntityProperty>().Property(p => p.EntityFullName).HasMaxLength(250);
+
+            //這裡用Player實體建立了名稱為Players的資料表，Map同理，加上s實際建立資料表時資料表名稱就會有s
+            //然後再最後加上 在建立模型時增加關聯性 Player->Map
+            //這樣就會有條件約束 Player內的MapID 必須在 Map 內可以找到對應的 ID
+            //到這邊基本 DbContext 就建立完成了
+            modelBuilder.Entity<Player>().HasRequired(p => p.Map);
         }
+
     }
 }
---
+++ MyCompany.MyProject.EntityFramework/Migrations/SeedData/DefaultMapsCreator.cs
@@ -0,0 +1,34 @@
+﻿using MyCompany.MyProject.Entities;
+using MyCompany.MyProject.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.MyProject.Migrations.SeedData
+{
+    class DefaultMapsCreator
+    {
+        private readonly MyProjectDbContext _context;
+        public DefaultMapsCreator(MyProjectDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            CreateMaps();
+        }
+
+        public void CreateMaps()
+        {
+            var defaultMap = _context.Maps.FirstOrDefault(t => t.MapName == Map.DefaultMapName);
+            if (defaultMap == null)
+            {
+                _context.Maps.Add(new Map { MapName = Map.DefaultMapName });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
