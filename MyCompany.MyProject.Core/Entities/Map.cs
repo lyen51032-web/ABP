63a533cdcdaaa9b0364691336a8b07008540889a
Jhang jia syun <lyen51032@gmail.com>
2021-03-15 11:39:12 +0800

    feat:建立Map/Player

---
+++ MyCompany.MyProject.Core/Entities/Map.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.MyProject.Entities
+{
+    public class Map : Entity<long>, IHasCreationTime
+    {
+        public const string DefaultMapName = "DefaultMap";
+
+        public virtual string MapName { get; set; }
+
+        public virtual DateTime CreationTime { get; set; }
+
+        public virtual ICollection<Player> Player { get; set; }
+
+        public Map()
+        {
+            CreationTime = DateTime.Now;
+        }
+    }
+}
---
+++ MyCompany.MyProject.Core/Entities/Player.cs
@@ -0,0 +1,28 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.MyProject.Entities
+{
+    public class Player : Entity<long>, IHasCreationTime
+    {
+        public const string DefaultPlayerName = "DefaultPlayer";
+
+        public virtual string PlayerName { get; set; }
+
+        public virtual long MapID { get; set; }
+
+        public virtual DateTime CreationTime { get; set; }
+
+        public virtual Map Map { get; set; }
+        public Player()
+        {
+            CreationTime = DateTime.Now;
+            MapID = 1;
+        }
+    }
+}
